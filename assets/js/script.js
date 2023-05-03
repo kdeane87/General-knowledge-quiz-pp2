11b344160147b76ba855ab5870524dda06e1a0d9
kdeane87 <kdeane87@gmail.com>
2023-05-03 13:40:16 +0000

    Add quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+/**
+ * List of questions and answers for the quiz
+ */
+
+
+let questions = [
+    {
+        question: "Blondie was the name of the dog belonging to which dictator?",
+        answers: {
+            a: 'Stalin',
+            b: 'Mussolini',
+            c: 'Hitler'
+          },
+          correctAnswer: 'c'
+        },
+        {
+            question: " Related to the llama, which South American mammal has long shaggy hair?",
+        answers: {
+            a: 'Alpaca',
+            b: 'Common opossum',
+            c: 'Silky anteater'
+          },
+          correctAnswer: 'a'
+
+        },
+        {
+            question: "According to the proverb, Necessity is the mother of…What?",
+            answers: {
+                a: 'Creation',
+                b: 'Discovery',
+                c: 'Invention'
+              },
+              correctAnswer: 'c'
+    
+            },
+            {
+                question: 'Which statesman is quoted as saying “From Stettin in the Baltic to Trieste in the Adriatic, an iron curtain has descended across the continent”?',
+                answers: {
+                    a: 'Micheal Collins',
+                    b: 'Winston Churchill',
+                    c: 'Ronald Reagan'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+                question: "In chemistry what is the smallest part of an element that can take part in a chemical reaction?",
+                answers: {
+                    a: 'Molecule',
+                    b: 'Atom',
+                    c: 'Electron'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+                question: "Which 'C' is an ornamental moulding running around the walls of a room near the ceiling?",
+                answers: {
+                    a: 'Cornice',
+                    b: 'Crevice',
+                    c: 'Corner'
+                  },
+                  correctAnswer: 'a'
+
+            },
+            {
+                question: "Ganymede is the largest moon of which planet?",
+                answers: {
+                    a: 'Jupiter',
+                    b: 'Saturn',
+                    c: 'Neptune'
+                  },
+                  correctAnswer: 'a'
+
+
+            },
+            {
+                question: "The Chinese year 2023 is represented by which animal?",
+                answers: {
+                    a: 'Mouse',
+                    b: 'Rabbit',
+                    c: 'Cat'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+                question: " Which American president became known as 'Tricky Dicky'?",
+                answers: {
+                    a: 'Trump',
+                    b: 'Clinton',
+                    c: 'Nixon'
+                  },
+                  correctAnswer: 'c'
+
+            },
+            {
+                question: "Which insect lives in a Formicary?",
+                answers: {
+                    a: 'Ant',
+                    b: 'Spider',
+                    c: 'Grasshopper'
+                  },
+                  correctAnswer: 'c'
+
+
+            }
+        
+    
+
+
+]
