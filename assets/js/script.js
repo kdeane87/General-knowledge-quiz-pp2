05887ae4bda0fd7232fbcce6ec6ba27ba147bb15
kdeane87 <kdeane87@gmail.com>
2023-05-03 15:42:13 +0000

    Add event listener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,6 +107,29 @@ let questions = [
             }
         
     
-
+            
 
 ]
+console.log(questions);
+
+// Wait for the DOM to finish loading before runing the game
+// Get the button elements and add event listeners to them
+
+
+document.addEventListener("DOMContentLoaded", function()) {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            checkAnswer();
+        } else {
+            let gameType = this.getAttribute("data-type");
+            runGame(gameType);
+        }
+    });
+ }
+}
+
+
+
