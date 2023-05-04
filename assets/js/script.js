ad722551fc5314be2371a7ecb34ba650cade69cf
kdeane87 <kdeane87@gmail.com>
2023-05-04 18:00:52 +0000

    Remove questions.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function runGame() {
+    const question = document.getElementById("q");
 
+    question.innerText = questions[id].q;
+
+    const op1 = document.getElementById('ans1');
+    const op2 = document.getElementById('ans2');
+    const op3 = document.getElementById('ans3');
+
+    op1.innerText = questions[id].a[0].text;
+    op2.innerText = questions[id].a[1].text;
+    op3.innerText = questions[id].a[2].text;
 }
+
+
 
 function checkAnswer() {
 
@@ -44,4 +56,123 @@ function displayMultipleChoice1() {
 }
 
 
-  
+  //List of questions and answers for the quiz
+
+const questions = [
+    {
+        id: 0,     
+        question: "Blondie was the name of the dog belonging to which dictator?",
+        answers: {
+            a: 'Stalin',
+            b: 'Mussolini',
+            c: 'Hitler'
+          },
+          correctAnswer: 'c'
+        },
+        {
+            id: 1,
+            question: " Related to the llama, which South American mammal has long shaggy hair?",
+        answers: {
+            a: 'Alpaca',
+            b: 'Common opossum',
+            c: 'Silky anteater'
+          },
+          correctAnswer: 'a'
+
+        },
+        {
+            id: 2,
+            question: "According to the proverb, Necessity is the mother of…What?",
+            answers: {
+                a: 'Creation',
+                b: 'Discovery',
+                c: 'Invention'
+              },
+              correctAnswer: 'c'
+    
+            },
+            {
+              id: 3,
+                question: 'Which statesman is quoted as saying “From Stettin in the Baltic to Trieste in the Adriatic, an iron curtain has descended across the continent”?',
+                answers: {
+                    a: 'Micheal Collins',
+                    b: 'Winston Churchill',
+                    c: 'Ronald Reagan'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+              id: 4,
+                question: "In chemistry what is the smallest part of an element that can take part in a chemical reaction?",
+                answers: {
+                    a: 'Molecule',
+                    b: 'Atom',
+                    c: 'Electron'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+              id: 5,
+                question: "Which 'C' is an ornamental moulding running around the walls of a room near the ceiling?",
+                answers: {
+                    a: 'Cornice',
+                    b: 'Crevice',
+                    c: 'Corner'
+                  },
+                  correctAnswer: 'a'
+
+            },
+            {
+              id: 6,
+                question: "Ganymede is the largest moon of which planet?",
+                answers: {
+                    a: 'Jupiter',
+                    b: 'Saturn',
+                    c: 'Neptune'
+                  },
+                  correctAnswer: 'a'
+
+
+            },
+            { id: 7,
+                question: "The Chinese year 2023 is represented by which animal?",
+                answers: {
+                    a: 'Mouse',
+                    b: 'Rabbit',
+                    c: 'Cat'
+                  },
+                  correctAnswer: 'b'
+
+            },
+            {
+              id: 8,
+                question: " Which American president became known as 'Tricky Dicky'?",
+                answers: {
+                    a: 'Trump',
+                    b: 'Clinton',
+                    c: 'Nixon'
+                  },
+                  correctAnswer: 'c'
+
+            },
+            {
+              id: 9,
+                question: "Which insect lives in a Formicary?",
+                answers: {
+                    a: 'Ant',
+                    b: 'Spider',
+                    c: 'Grasshopper'
+                  },
+                  correctAnswer: 'c'
+
+
+            }
+        
+    
+            
+
+]
+console.log(questions[0].question);
+console.log(questions[4].correctAnswer);
