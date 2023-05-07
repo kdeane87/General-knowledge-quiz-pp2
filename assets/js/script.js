d2a3925790497ba3dd773abb76dae3b65fa7f8f7
kdeane87 <kdeane87@gmail.com>
2023-05-07 12:43:52 +0000

    Comment out some css until later

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,60 @@ const questionArea = document.getElementById("questions");
 const correctScore = document.getElementById("score");
 const incorrectScore = document.getElementById("incorrect");
 const questionContainerElement = document.getElementById('question-container');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
  //List of questions and answers for the quiz
 
- const questions = [
+ 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "a") {
+              alert("You clicked Answer a!");
+          } else {
+              let choice = this.getAttribute("data-type");
+              alert(`You clicked Answer ${choice}!`);
+          }
+      });
+  }
+});
+
+
+
+
+function runGame() {
+  
+}
+
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function displayMultipleChoice1() {
+
+}
+
+
+
+const questions = [
   {
       id: 0,     
       question: "Blondie was the name of the dog belonging to which dictator?",
@@ -124,53 +174,4 @@ const questionContainerElement = document.getElementById('question-container');
           
 
 ]
-
-// Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
-
-document.addEventListener("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName("button");
-
-  for (let button of buttons) {
-      button.addEventListener("click", function() {
-          if (this.getAttribute("data-type") === "a") {
-              alert("You clicked Answer a!");
-          } else {
-              let choice = this.getAttribute("data-type");
-              alert(`You clicked Answer ${choice}!`);
-          }
-      });
-  }
-});
-
-
-
-
-function runGame() {
- 
-}
-
-
-function checkAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displayQuestion() {
-
-}
-
-function displayMultipleChoice1() {
-
-}
-
-
-
 
