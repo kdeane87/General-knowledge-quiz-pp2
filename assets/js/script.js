e68134e6c86006fde5f7945d3b1bd1a49c16a95f
kdeane87 <kdeane87@gmail.com>
2023-05-10 13:39:29 +0000

    Add class correct/wrong to btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,6 +104,11 @@ function displayQuestion() {
 function checkAnswer(e) {
   const selectedBtn = e.target;
   const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("wrong");
+  }
 }
 
 function incrementScore() {}
